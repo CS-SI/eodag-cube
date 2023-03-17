5d414e75809a9051efccf7f81660dcf20ea55c81
Sylvain Brunato <61419125+sbrunato@users.noreply.github.com>
2023-03-17 10:14:35 +0100

    fix: update precommit and dependencies (#33)

--- eodag_cube/api/product/protobuf/__init__.py
+++ eodag_cube/api/product/protobuf/__init__.py
@@ -16,3 +16,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """EODAG product protobuf package"""
+import os
+
+# Fixes: descriptors cannot not be created directly.
+# If this call came from a _pb2.py file, your generated code is out of date and must be regenerated with protoc >= 3.19.0.
+# If you cannot immediately regenerate your protos, some other possible workarounds are:
+#  1. Downgrade the protobuf package to 3.20.x or lower.
+#  2. Set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python (but this will use pure-Python parsing and will be much slower).
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
