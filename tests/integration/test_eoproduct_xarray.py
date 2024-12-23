8eb018aa61f3b2e2e10aa1e53b0c66625674b408
Sylvain Brunato <sylvain.brunato@c-s.fr>
2024-12-23 16:48:56 +0100

    test: do not copy products to tmpdir

--- tests/integration/test_eoproduct_xarray.py
+++ tests/integration/test_eoproduct_xarray.py
@@ -16,43 +16,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import sys
 from pathlib import Path
-from tempfile import TemporaryDirectory
 
 import xarray as xr
 
 from eodag_cube.types import XarrayDict
 from tests import EODagTestCase
-from tests.context import AwsDownload, EOProduct, PluginConfig, path_to_uri
-from tests.utils import populate_directory_with_heterogeneous_files
+from tests.context import (
+    TEST_RESOURCES_PATH,
+    AwsDownload,
+    EOProduct,
+    PluginConfig,
+    path_to_uri,
+)
 
 
 class TestEOProductXarray(EODagTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestEOProductXarray, cls).setUpClass()
-        cls.tmp_dir = TemporaryDirectory(prefix="eodag-cube-tests")
-
-    @classmethod
-    def tearDownClass(cls):
-        super(TestEOProductXarray, cls).tearDownClass()
-        cls.tmp_dir.cleanup()
-
     def test_to_xarray_local(self):
         """to_xarray must build a Dataset from found local paths"""
         product = EOProduct(
             self.provider, self.eoproduct_props, productType=self.product_type
         )
         product.register_downloader(AwsDownload("foo", PluginConfig()), None)
-        product.location = path_to_uri(self.tmp_dir.name)
-        populate_directory_with_heterogeneous_files(self.tmp_dir.name)
+
+        products_path = os.path.join(
+            TEST_RESOURCES_PATH,
+            "products",
+        )
+        product.location = path_to_uri(products_path)
 
         xarray_dict = product.to_xarray()
 
         self.assertIsInstance(xarray_dict, XarrayDict)
-        self.assertEqual(len(xarray_dict), 2)
+        # cfgrib only available for python <= 3.9
+        if sys.version_info.minor <= 9:
+            self.assertEqual(len(xarray_dict), 3)
+        else:
+            self.assertEqual(len(xarray_dict), 2)
         for key, value in xarray_dict.items():
-            self.assertIn(Path(key).suffix, {".nc", ".jp2"})
+            # cfgrib only available for python <= 3.9
+            if sys.version_info.minor <= 9:
+                self.assertIn(Path(key).suffix, {".nc", ".grib", ".jp2"})
+            else:
+                self.assertIn(Path(key).suffix, {".nc", ".jp2"})
             self.assertIsInstance(value, xr.Dataset)
 
         for k, ds in xarray_dict.items():
